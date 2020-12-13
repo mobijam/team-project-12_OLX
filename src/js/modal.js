0861d6c2014f24076729a6611714c3d2b6073147
Viktoriia Strelnikova <v.strelnikova91@gmail.com>
2020-12-14 00:02:40 +0200

    add actual modals and fix markup

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,63 +1,101 @@
-const refs = {
+import checkType from './chooseModal';
+
+export const refs = {
   openModalSearchBtn: document.querySelector('[data-modal-search]'),
-  openModalSearchMobileBtn: document.querySelector(
-    '[data-modal-search-mobile]',
-  ),
-  openModalLoginBtn: document.querySelector('[data-modal-login]'),
-  openModalLoginMobileBtn: document.querySelector(
-    '[data-action="mobile-register"]',
-  ),
-  openModalCreateBtn: document.querySelector('[data-modal-create]'),
-  openModalCreateMobileBtn: document.querySelector(
-    '[data-modal-create-mobile]',
-  ),
-  closeModalBtn: document.querySelector('[data-modal-close]'),
+  openModalSearchMobileBtn: document.querySelector('[data-modal-search-mobile]'),
+
+  // openModalLoginBtn: document.querySelector('[data-modal-login]'),
+  // openModalLoginMobileBtn: document.querySelector(
+  //   '[data-action="register-mobile"]',
+  // ),
+  // openModalCreateBtn: document.querySelector('[data-modal-create]'),
+  // openModalCreateMobileBtn: document.querySelector(
+  //   '[data-modal-create-mobile]',
+  // ),
+
+  closeModalBtn: document.querySelector('[data-action="modal-close"]'),
 
   modal: document.querySelector('[data-modal]'),
   modalContent: document.querySelector('[data-modal-content]'),
+  modalForm: document.querySelector('[data-modal-form]'),
 };
 
-refs.openModalSearchBtn.addEventListener('click', () => {
-  toggleModal();
-  refs.modalContent.setAttribute('action', 'search');
-  refs.modalContent.children[1].textContent = 'МОДАЛОЧКА ПОИСКА';
-});
 
-refs.openModalSearchMobileBtn.addEventListener('click', () => {
-  toggleModal();
-  refs.modalContent.setAttribute('action', 'search');
-  refs.modalContent.children[1].textContent = 'МОДАЛОЧКА ПОИСКА';
-});
-refs.openModalLoginBtn.addEventListener('click', () => {
-  toggleModal();
-  refs.modalContent.setAttribute('action', 'login');
-  refs.modalContent.children[1].textContent = 'МОДАЛОЧКА АВТОРИЗАЦИИ';
-});
-refs.openModalLoginMobileBtn.addEventListener('click', () => {
-  toggleModal();
-  refs.modalContent.setAttribute('action', 'login');
-  refs.modalContent.children[1].textContent = 'МОДАЛОЧКА АВТОРИЗАЦИИ';
-});
+export default function openModal(e) {
+  // checkLogin();
 
-refs.openModalCreateBtn.addEventListener('click', () => {
+  let type = '';
+  if (
+    e.currentTarget.hasAttribute('data-modal-search') ||
+    e.currentTarget.hasAttribute('data-modal-search-mobile')
+  ) {
+    type = 'search';
+  }
+  if (
+    e.currentTarget.hasAttribute('data-modal-create') ||
+    e.currentTarget.hasAttribute('data-modal-create-mobile')
+  ) {
+    type = 'create';
+  }
+
+  if (
+    e.currentTarget.hasAttribute('data-modal-login') ||
+    e.currentTarget.hasAttribute('data-modal-login-mobile')
+  ) {
+    type = 'login';
+  }
+
   toggleModal();
-  refs.modalContent.setAttribute('action', 'create');
-  refs.modalContent.children[1].textContent = 'МОДАЛОЧКА СОЗДАНИЯ ОБЪЯВЛЕНИЯ';
-});
-refs.openModalCreateMobileBtn.addEventListener('click', () => {
-  toggleModal();
-  refs.modalContent.setAttribute('action', 'create');
-  refs.modalContent.children[1].textContent = 'МОДАЛОЧКА СОЗДАНИЯ ОБЪЯВЛЕНИЯ';
-});
+  refs.modalContent.setAttribute('action', `${type}`);
+  checkType();
+  refs.modalContent.children[1].textContent = ` `;
+}
+
+
+refs.openModalSearchBtn.addEventListener('click', openModal);
+refs.openModalSearchMobileBtn.addEventListener('click', openModal);
+
+// refs.openModalLoginBtn.addEventListener('click', openModal);
+// refs.openModalLoginMobileBtn.addEventListener('click', openModal);
+// refs.openModalLoginBtn.addEventListener('click', checkLogin);
+
+// refs.openModalCreateBtn.addEventListener('click', openModal);
+// refs.openModalCreateMobileBtn.addEventListener('click', openModal);
+
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
+
 function toggleModal() {
+  refs.modalForm.innerHTML = '';
   if (refs.modalContent.hasAttribute('action')) {
     refs.modalContent.removeAttribute('action');
   }
-
   document.body.classList.toggle('modal-open');
   refs.modal.classList.toggle('is-hidden');
   document.body.classList.toggle('no-scroll');
 }
-// console.dir(refs.modalContent.children[1].textContent);
+
+
+// function checkLogin() {
+//   const checkAuth = localStorage.getItem('key');
+
+//   if (checkAuth !== null) {
+//     refs.openModalLoginBtn.removeEventListener('click', openModal);
+//     refs.openModalLoginMobileBtn.removeEventListener('click', openModal);
+//     refs.openModalCreateBtn.removeAttribute('data-modal-login');
+//     refs.openModalCreateBtn.setAttribute('data-modal-create', '');
+//     refs.openModalCreateMobileBtn.removeAttribute('data-modal-login-mobile');
+//     refs.openModalCreateMobileBtn.setAttribute('data-modal-create-mobile', '');
+//   }
+//   if (checkAuth === null) {
+//     refs.openModalCreateBtn.removeAttribute('data-modal-create');
+//     refs.openModalCreateBtn.setAttribute('data-modal-login', '');
+//     refs.openModalCreateMobileBtn.removeAttribute('data-modal-create-mobile');
+//     refs.openModalCreateMobileBtn.setAttribute('data-modal-login-mobile', '');
+//     refs.openModalLoginBtn.addEventListener('click', openModal);
+//     refs.openModalLoginMobileBtn.addEventListener('click', openModal);
+//   }
+// }
+
+
+// checkLogin();
