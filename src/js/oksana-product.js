3d86c210277f0ea6947d8e56abf4a733de4fccf5
Oksana Kochenko <oksanakochenkoo@gmail.com>
2020-12-20 16:32:17 +0200

    добавлен запрос информ-и с бека о продукте

--- src/js/oksana-product.js
+++ src/js/oksana-product.js
@@ -1,9 +1,13 @@
 import userInfoTpl from '../templates/user-info.hbs';
+import productCardTpl from '../templates/product-card.hbs';
 const BASE_URL = 'https://callboard-backend.herokuapp.com';
 const refs = {
   userContainer: document.querySelector('.js-user-container'),
   userBtn: document.querySelector('.js-user-button'),
+  cardContainer: document.querySelector('.js-product-card'),
+  cardBtn: document.querySelector('.'),
 };
+
 userBtn.addEventListener('click', onUserBtnClick);
 
 function onUserBtnClick(e) {
@@ -23,3 +27,16 @@ function renderUserInfo(userId) {
 }
 
 export default { userInfo };
+
+cardBtn.addEventListener('click', onCardBtnClick);
+function onCardBtnClick(e) {
+  e.preventDefault();
+}
+
+function cardInfo(callId) {
+  return fetch(`${BASE_URL}/call{callId}`).then(responce => Response.json());
+}
+
+function renderProductCard(cardInfo) {
+  refs.cardContainer.innerHTML = productCardTpl(callId);
+}
