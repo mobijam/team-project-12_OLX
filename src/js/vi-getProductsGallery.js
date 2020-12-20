a8428a378c1eda2e14f37bc417b1cf032638c2f7
Viktoriia Strelnikova <56576729+mobijam@users.noreply.github.com>
2020-12-20 17:22:39 +0200

    Revert "добавлены иконки навигации и на картинках"

--- src/js/vi-getProductsGallery.js
+++ src/js/vi-getProductsGallery.js
@@ -1,86 +1,52 @@
-/* 
-
-. приходит response.json в виде объекта из 5 массивов объектов
-. каждый объект массива содержит :
-, - category
-, - description
-, - imageUrls
-, - isOnSale
-, - phone
-, - price
-, - title
-, - userId
-, - __v
-, - _id 
-
-
-*/
 
 // ------------------BEGIN----------------
-import markUpList from '../templates/vi-category-list.hbs';
+import markUpList from '../templates/vi-category-list.hbs'
 import GetProductGallery from './vi-apiService';
 const getProductGallery = new GetProductGallery();
 const refs = getRefs();
-const getPageNumber = (event) => event.target.outerText;
 
-//| ---------------------tests-------------------
-// | --------------------------------------------
+refs.changePage.forEach(element => {
+    element.addEventListener('click', getNewFething);
+});
+    
+getProductList();
 
-if (window.innerWidth <= 768) { refs.mobileButton.addEventListener('click', getNewFething) };
-
-refs.changePage.forEach(element => element.addEventListener('click', getNewFething));
-
-getCategorytList()
 
 function getNewFething(e) {
     clearContainer()
     getProductGallery.pageNumber = getPageNumber(e);
-    // getProductGallery.rusRequest = '/russian-categories';
-    getCategorytList();
+    console.log( 'current Page-->',  getProductGallery.pageNumber);
+    getProductList()
+
 }
  
-function getCategorytList() {
-    return getProductGallery.fetchCategoryList()
+function getPageNumber(event) {
+    console.log( 'pyfxtybt ryjgr lhblskjbdfm', event.target.outerText);
+   return event.target.outerText;
+
+}
+
+function getProductList() {
+    return getProductGallery.fetchProduct()
         .then(response => {
-            return response
+            // console.log('response - > ', response);
+        return response
         })
-        .then(parceCategoryList);
-}
-    
-
-function getCategoryPage(e) {
-    getProductGallery.newRequest = 'property';
-    console.log('Вы перешли на страницу КАТЕГОРИИ');
-    getProductGallery.fetchCategoryContent();
-}
-
-function addToFavorite(e) {
-    console.log('Вы перешли на страницу ИЗБРАННЫЕ');
+        .then(parceCategoryList)
 }
 
 function parceCategoryList(arg) {
     const listOfProduct = markUpList(arg);
-    refs.categoryList.insertAdjacentHTML("afterbegin", listOfProduct);
-    const categoryPageLink = document.querySelectorAll('.category-navi .text');
-    const favoriteButtons = document.querySelectorAll('[data-action="favorite"]') 
-    categoryPageLink.forEach(el => el.addEventListener('click', getCategoryPage));
-    favoriteButtons.forEach(e=>e.addEventListener('click', addToFavorite))
-    
+    refs.mainContainer.insertAdjacentHTML("afterbegin", listOfProduct);
 }
-
 
 function getRefs() {
     return {
-        // mainContainer: document.querySelector('.category-list-container'),
-        // mainContainer: document.querySelector('.js-categories'),
-        categoryList: document.querySelector('.category-list'),
+        mainContainer: document.querySelector('.category-list-container'),
+        categoryList: document.createElement('ul'),
         changePage: document.querySelectorAll('.js-page-number'),
-        mobileButton: document.querySelector('[data-action="data-modal-create-mobile"]'),
-        // categoryPageLink: document.querySelector('.category-navi .text'),
     }
 }
-    
-function clearContainer() {refs.categoryList.innerHTML = ''};
-
-export function fetchOnError(arg = 'ERROR') {console.log(arg)}
- 
+function clearContainer() {
+     refs.mainContainer.innerHTML=''
+ }
